f6d8065cb1f8781d0933b6cf34ea05d009a5fe0e
user <KurbatovVV82@mail.ru>
2023-10-22 12:01:58 +0300

    before FSM for ssl session ref

--- tools/ssl/ssl_session_manager.h
+++ tools/ssl/ssl_session_manager.h
@@ -2,14 +2,14 @@
 #define SSL_SESSION_MANAGER_H
 
 #include "i_ssl_session.h"
-
+#include "i_ssl_session_manager.h"
 
 namespace ssl
 {
 
 struct ssl_manager_config_t;
 
-class ssl_session_manager
+class ssl_session_manager : public i_ssl_session_manager
 {
 private:
     struct pimpl_t;
@@ -21,10 +21,10 @@ public:
     ssl_session_manager(const ssl_manager_config_t& config);
     ~ssl_session_manager();
 
-    const i_ssl_certificate* certificate() const;
+    const i_ssl_certificate* certificate() const override;
 
     i_ssl_session::u_ptr_t create_session(const ssl_session_params_t& params
-                                          , i_ssl_session::i_listener* listener = nullptr);
+                                          , i_ssl_session::i_listener* listener = nullptr) override;
 
     bool is_valid() const;
 };
